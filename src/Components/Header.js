216ab801a837ff1e6f0994225c842dde12d3b40d
brunokian <brunokian@hotmail.com>
2022-12-18 00:15:46 -0300

    test

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,16 @@
 import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return(
     <div className="header">
-      <img className="pizza-logo" src="pizza-logo.png" alt="logo"/>
+      <Link to="/">
+        <img className="pizza-logo" src="pizza-logo.png" alt="logo"/>
+      </Link>
       <motion.h1 className="tittle"
         initial={{ y: -250 }}
         animate={{ y: -10 }}
-        transition={{ delay: 0.3, type: 'spring', stiffness: 100 }}
+        transition={{ delay: 0.3, type: 'spring', stiffness: 90 }}
       >PIZZA DELIVERY</motion.h1>
     </div>
   )
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,22 @@
 import { Link } from "react-router-dom";
 import { motion } from 'framer-motion'
 
+const buttonVariants = {
+  visible: {
+    x: -20,
+    transition: { delay: 2 }
+  },
+  hover: {
+    scale: 1.1,
+    textShadow: '0px 0px 8px rgb(255, 255, 255)',
+    boxShadow: '0px 0px 8px rgb(255, 255, 255)',
+    transition: {
+      yoyo: Infinity,
+      duration: 0.3,
+    }
+  }
+}
+
 const Home = () => {
   return(
     <motion.div className="home container"
@@ -11,11 +27,9 @@ const Home = () => {
       <h1>Welcome to Delivery Pizza!</h1>
       <Link to="/base">
         <motion.button
-          whileHover={{ 
-            scale: 1.1,
-            textShadow: '0px 0px 8px rgb(255, 255, 255)',
-            boxShadow: '0px 0px 8px rgb(255, 255, 255)'
-          }}
+          variants={buttonVariants}
+          animate="visible"
+          whileHover="hover"
         >Create your pizza</motion.button>
       </Link>
     </motion.div>
